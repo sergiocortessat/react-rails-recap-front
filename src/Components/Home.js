47828391ea5e30a6ccf1101a1eab3a9029007cd4
Sergio Cortes <sergiocortessat@gmail.com>
2021-08-19 12:00:52 +0200

    Add movies

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import FsLightbox from 'fslightbox-react';
 import { getPosts, postPost, deletePost } from '../API';
 
 const Home = () => {
@@ -35,6 +36,8 @@ const Home = () => {
       setPosts((prev) => prev.filter((p) => p.id !== id));
     });
   };
+
+  const [toggler, setToggler] = useState(false);
 
   useEffect(() => {
     getPosts().then((response) => {
@@ -77,8 +80,53 @@ const Home = () => {
           {isAuthenticated && (post.sub === user.sub) && (
           <button type="button" onClick={() => handleDelete(post)}>Delete</button>
           )}
+
         </div>
       ))}
+
+      <button type="button" onClick={() => setToggler(!toggler)}>
+        Toggle Lightbox
+      </button>
+      <FsLightbox
+        toggler={toggler}
+        sources={[
+          <iframe
+            key="1"
+            id="iframe"
+            title="yes"
+            src="https://www.2embed.ru/embed/tmdb/movie?id=664031"
+            width="1920px"
+            height="1080px"
+            frameBorder="0"
+            allow="autoplay; fullscreen"
+            allowFullScreen
+          />,
+          <iframe
+            src="https://player.vimeo.com/video/22439234"
+            width="1920px"
+            height="1080px"
+            frameBorder="0"
+            allow="autoplay; fullscreen"
+            allowFullScreen
+            key="2"
+            title="no"
+          />,
+
+          <iframe
+            src="https://www.2embed.ru/embed/tmdb/movie?id=550988"
+            width="1920px"
+            height="1080px"
+            frameBorder="0"
+            allow="autoplay; fullscreen"
+            allowFullScreen
+            key="3"
+            title="nono"
+          />,
+
+        ]}
+      />
+      {/* <iframe id="iframe" src="https://www.2embed.ru/embed/tmdb/movie?id=550988" width="100%" height="100%" frameborder="0"></iframe> */}
+      {/* <iframe id="iframe" title="yes" src="https://www.2embed.ru/embed/tmdb/movie?id=664031" width="100%" height="100px" allowFullScreen frameBorder="0" /> */}
     </div>
   );
 };
